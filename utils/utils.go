b6c99cafbd16a68e3fee0659b43137304d412428
Gregor Pifko <gregor.pifko@gmail.com>
2025-05-28 00:48:03 +0200

    utils: getInput, GetString, GetInt and GetUint63

---
+++ utils/utils.go
@@ -0,0 +1,52 @@
+package utils
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func getInput(prompt string) string {
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print(prompt)
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading input. Please try again.")
+			continue
+		}
+
+		return strings.TrimSpace(input)
+	}
+}
+
+func GetString(prompt string) string {
+	return getInput(prompt)
+}
+
+func GetInt(prompt string) int {
+	for {
+		input := getInput(prompt)
+		number, err := strconv.Atoi(input)
+		if err != nil {
+			fmt.Println("Invalid integer. Please try again.")
+			continue
+		}
+		return number
+	}
+}
+
+func GetUint64(prompt string) uint64 {
+	for {
+		input := getInput(prompt)
+		number, err := strconv.ParseUint(input, 10, 64)
+		if err != nil {
+			fmt.Println("Invalid integer. Please try again.")
+			continue
+		}
+		return uint64(number)
+	}
+}
